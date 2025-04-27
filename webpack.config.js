c2b46b9ee254fa350b23a28a4f138e6b4d0b6df1
Konstantin Korovin <cosmo.stan@yandex.com>
2025-04-27 17:49:35 +0300

    build: add config for webpack

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const sass = require('sass');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  mode: isProduction ? 'production' : 'development',
+  devtool: isProduction ? false : 'source-map',
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: isProduction ? '[name].[contenthash].js' : 'bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: sass,
+              sassOptions: {
+                quietDeps: true,
+                outputStyle: isProduction ? 'compressed' : 'expanded',
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/public/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    open: true,
+    hot: true,
+    proxy: [
+      {
+        context: ['/api', '/auth'],
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        logLevel: 'debug',
+      },
+    ],
+  },
+};
