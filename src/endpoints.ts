9696ea99a202dec2d9fc455ab1d8479a351fbf80
Konstantin Korovin <cosmo.stan@yandex.com>
2025-04-30 21:17:24 +0300

    feat: add endpoints for routing

---
+++ src/endpoints.ts
@@ -0,0 +1,11 @@
+enum Endpoints {
+  Home = '/',
+  Login = '/login',
+  SignUp = '/signup',
+  Users = '/users',
+  Statuses = '/statuses',
+  Labels = '/labels',
+  Tasks = '/tasks',
+}
+
+export default Endpoints;
