74f1c8e4661b954a4bd2bb8a338abb6f329d5701
Konstantin Korovin <cosmo.stan@yandex.com>
2025-04-30 21:51:51 +0300

    refactor: change name Routes to Endpoints

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import cn from 'classnames';
-import Routes from '@/endpoints';
+import Endpoints from '@/endpoints';
 
 export default function Header() {
   const linkClasses = cn('nav-link', 'px-2', 'link-dark');
@@ -10,20 +10,20 @@ export default function Header() {
     <header className="bg-white mb-4">
       <div className="container">
         <nav className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3">
-          <Link to={Routes.Home} className="col-md-3 mb-2 mb-md-0 text-dark text-decoration-none fs-4">
+          <Link to={Endpoints.Home} className="col-md-3 mb-2 mb-md-0 text-dark text-decoration-none fs-4">
             <i className="bi bi-card-checklist" />
             {' '}
             Task Manager
           </Link>
           <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><NavLink to={Routes.Users} className={linkClasses}>Users</NavLink></li>
-            <li><NavLink to={Routes.Statuses} className={linkClasses}>Statuses</NavLink></li>
-            <li><NavLink to={Routes.Labels} className={linkClasses}>Labels</NavLink></li>
-            <li><NavLink to={Routes.Tasks} className={linkClasses}>Tasks</NavLink></li>
+            <li><NavLink to={Endpoints.Users} className={linkClasses}>Users</NavLink></li>
+            <li><NavLink to={Endpoints.Statuses} className={linkClasses}>Statuses</NavLink></li>
+            <li><NavLink to={Endpoints.Labels} className={linkClasses}>Labels</NavLink></li>
+            <li><NavLink to={Endpoints.Tasks} className={linkClasses}>Tasks</NavLink></li>
           </ul>
           <div className="col-md-3 text-end">
-            <Link to={Routes.Login} className="btn btn-outline-primary me-2">Login</Link>
-            <Link to={Routes.SignUp} className="btn btn-primary">Sign-up</Link>
+            <Link to={Endpoints.Login} className="btn btn-outline-primary me-2">Login</Link>
+            <Link to={Endpoints.SignUp} className="btn btn-primary">Sign-up</Link>
           </div>
         </nav>
       </div>
