b519c976150f16da4dc70d0a1ff2bbb3a5ae503a
Konstantin Korovin <cosmo.stan@yandex.com>
2025-05-09 21:39:52 +0300

    chore: add config for knex

---
+++ knexfile.ts
@@ -0,0 +1,44 @@
+import path from 'path';
+import type { Knex } from 'knex';
+import { knexSnakeCaseMappers } from 'objection';
+
+const migrations = {
+  directory: path.resolve(__dirname, 'server', 'migrations'),
+  extension: 'ts',
+};
+
+const config: { [key: string]: Knex.Config } = {
+  production: {
+    client: 'pg',
+    connection: {
+      database: process.env.PG_DB,
+      host: process.env.PG_HOST,
+      port: Number(process.env.PG_PORT) || 5432,
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      ssl: true,
+    },
+    useNullAsDefault: true,
+    migrations,
+    ...knexSnakeCaseMappers(),
+  },
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, 'db.sqlite'),
+    },
+    useNullAsDefault: true,
+    migrations,
+    ...knexSnakeCaseMappers(),
+  },
+  test: {
+    client: 'sqlite3',
+    connection: ':memory:',
+    useNullAsDefault: true,
+    debug: true,
+    migrations,
+    ...knexSnakeCaseMappers(),
+  },
+};
+
+export default config;
