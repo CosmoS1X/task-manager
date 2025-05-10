4c453c6b6d2c4f1d01acfdbc16520f0dd3d167b7
Konstantin Korovin <cosmo.stan@yandex.com>
2025-05-10 19:38:39 +0300

    feat: add 'User' model

---
+++ server/models/User.ts
@@ -0,0 +1,38 @@
+import { Model } from '../database';
+import encrypt from '../lib/secure';
+
+export default class User extends Model {
+  id!: number;
+
+  firstName!: string;
+
+  lastName!: string;
+
+  email!: string;
+
+  passwordDigest!: string;
+
+  createdAt!: Date;
+
+  static get tableName() {
+    return 'users';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['firstName', 'lastName', 'email', 'password'],
+      properties: {
+        id: { type: 'integer' },
+        firstName: { type: 'string', minLength: 2, maxLength: 255 },
+        lastName: { type: 'string', minLength: 2, maxLength: 255 },
+        email: { type: 'string', format: 'email' },
+        password: { type: 'string', minLength: 5, maxLength: 255 },
+      },
+    };
+  }
+
+  set password(password: string) {
+    this.passwordDigest = encrypt(password);
+  }
+}
