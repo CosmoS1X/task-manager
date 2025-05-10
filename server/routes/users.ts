c6e2e59c2b24ee655225a1a5b01f331f08adef95
Konstantin Korovin <cosmo.stan@yandex.com>
2025-05-10 19:40:45 +0300

    feat: add routes for users CRUD

---
+++ server/routes/users.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import userController from '../controllers/userController';
+
+const router = Router();
+const controller = userController();
+
+router.get('/users', controller.getList);
+router.get('/users/:id', controller.getItem);
+router.post('/users', controller.create);
+router.patch('/users/:id', controller.update);
+router.delete('/users/:id', controller.delete);
+
+export default router;
