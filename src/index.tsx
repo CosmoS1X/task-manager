e53d1164eaaec284339217249686e736ee7ba5ea
Konstantin Korovin <cosmo.stan@yandex.com>
2025-05-11 18:21:55 +0300

    feat: connect i18next to the project

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,24 @@ import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import resources from '@/locales';
 import App from './components/App';
 import store from '@/services';
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    supportedLngs: ['en', 'ru'],
+    fallbackLng: 'en',
+    resources,
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  });
 
 const mountNode = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(mountNode);
