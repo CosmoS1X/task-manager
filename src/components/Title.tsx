315db2e4b55ebb79445cd5ce90777e03b85a429e
Konstantin Korovin <cosmo.stan@yandex.com>
2025-05-11 18:32:28 +0300

    refactor: edit props validation in Title component

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { z } from 'zod';
 
-const propsSchema = z.object({
-  text: z.string(),
-});
-
-type Props = z.infer<typeof propsSchema>;
+type Props = {
+  text: string;
+};
 
 export default function Title({ text }: Props) {
   return <h1 className="display-4 fw-bold mt-4">{text}</h1>;
