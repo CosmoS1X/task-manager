cf900b7d139504e76d8da59ad7ec61952ad0376d
Konstantin Korovin <cosmo.stan@yandex.com>
2025-05-11 18:27:28 +0300

    feat: output texts via i18next

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import cn from 'classnames';
 import Endpoints from '@/endpoints';
 
 export default function Header() {
+  const { t } = useTranslation();
   const linkClasses = cn('nav-link', 'px-2', 'link-dark');
 
   return (
@@ -13,17 +15,17 @@ export default function Header() {
           <Link to={Endpoints.Home} className="col-md-3 mb-2 mb-md-0 text-dark text-decoration-none fs-4">
             <i className="bi bi-card-checklist" />
             {' '}
-            Task Manager
+            {t('appName')}
           </Link>
           <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><NavLink to={Endpoints.Users} className={linkClasses}>Users</NavLink></li>
-            <li><NavLink to={Endpoints.Statuses} className={linkClasses}>Statuses</NavLink></li>
-            <li><NavLink to={Endpoints.Labels} className={linkClasses}>Labels</NavLink></li>
-            <li><NavLink to={Endpoints.Tasks} className={linkClasses}>Tasks</NavLink></li>
+            <li><NavLink to={Endpoints.Users} className={linkClasses}>{t('navbar.users')}</NavLink></li>
+            <li><NavLink to={Endpoints.Statuses} className={linkClasses}>{t('navbar.statuses')}</NavLink></li>
+            <li><NavLink to={Endpoints.Labels} className={linkClasses}>{t('navbar.labels')}</NavLink></li>
+            <li><NavLink to={Endpoints.Tasks} className={linkClasses}>{t('navbar.tasks')}</NavLink></li>
           </ul>
           <div className="col-md-3 text-end">
-            <Link to={Endpoints.Login} className="btn btn-outline-primary me-2">Login</Link>
-            <Link to={Endpoints.SignUp} className="btn btn-primary">Sign-up</Link>
+            <Link to={Endpoints.Login} className="btn btn-outline-primary me-2">{t('navbar.signIn')}</Link>
+            <Link to={Endpoints.SignUp} className="btn btn-primary">{t('navbar.signUp')}</Link>
           </div>
         </nav>
       </div>
