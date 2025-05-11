c13f788b17eaf0aee4f288f94f2a21d0f7f8e257
Konstantin Korovin <cosmo.stan@yandex.com>
2025-05-11 18:20:49 +0300

    feat: connect store to the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './components/App';
+import store from '@/services';
 
 const mountNode = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(mountNode);
 
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 );
