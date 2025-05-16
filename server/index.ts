4a8d27cdb43504a5c7834e5bbd5511d102e3a12d
Konstantin Korovin <cosmo.stan@yandex.com>
2025-05-16 22:01:52 +0300

    feat: implement auth via session and passport

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,46 @@
 import express from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
+import session from 'express-session';
 import morgan from 'morgan';
 import path from 'path';
+import passport from './lib/passport';
+import sessionRouter from './routes/session';
 import usersRouter from './routes/users';
+
+const isProduction = process.env.ENV_NODE === 'production';
 
 const app = express();
 
 app.use(morgan('dev'));
-app.use(cors());
-app.use(bodyParser.json());
-app.use('/api', usersRouter);
 
-if (process.env.NODE_ENV === 'production') {
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
+
+app.use(session({
+  secret: process.env.SESSION_KEY || 'cdf49a999da33791f7646f7c5e5874c66b1247c5db7cc77e9d6a7844430cfc43d98f1160eeef3a91b2c6409ae9b4147f8920ec5fa6743b4d82700b310a918a3a',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: isProduction,
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
+    httpOnly: true,
+    sameSite: 'strict',
+  },
+  unset: 'destroy',
+  rolling: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.json());
+
+app.use('/api', usersRouter);
+app.use('/api', sessionRouter);
+
+if (isProduction) {
   const clientPath = path.join(__dirname, '..');
 
   app.use(express.static(clientPath));
