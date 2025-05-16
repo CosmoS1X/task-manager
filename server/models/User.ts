e6a27d9d06864eb8acfa5adbdcba785b79bf73dd
Konstantin Korovin <cosmo.stan@yandex.com>
2025-05-16 19:30:46 +0300

    feat: add 'verifyPassword' method to 'User' model

--- server/models/User.ts
+++ server/models/User.ts
@@ -35,4 +35,8 @@ export default class User extends Model {
   set password(password: string) {
     this.passwordDigest = encrypt(password);
   }
+
+  verifyPassword(password: string) {
+    return encrypt(password) === this.passwordDigest;
+  }
 }
