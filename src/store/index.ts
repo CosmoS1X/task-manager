9a6060968a89afc1a960da30fdadb74e4912f30b
Konstantin Korovin <cosmo.stan@yandex.com>
2025-05-17 22:44:16 +0300

    feat: connect authApi and authSlice to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { usersApi } from '@/services/usersApi';
+import { useDispatch, useSelector } from 'react-redux';
+import type { TypedUseSelectorHook } from 'react-redux';
+import { authApi } from '@/api/authApi';
+import { usersApi } from '@/api/usersApi';
+import authReducer from './authSlice';
 
-export default configureStore({
+export const store = configureStore({
   reducer: {
+    [authApi.reducerPath]: authApi.reducer,
     [usersApi.reducerPath]: usersApi.reducer,
+    auth: authReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(usersApi.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .concat(authApi.middleware)
+    .concat(usersApi.middleware),
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
