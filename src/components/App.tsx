950b748f4fcf00f3fa0c0c8548b752fe72f40fad
Konstantin Korovin <cosmo.stan@yandex.com>
2025-05-18 22:52:13 +0300

    feat: implement protected routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProtectedLayout from './ProtectedLayout';
 import Endpoints from '@/endpoints';
 import Header from './Header';
 import Footer from './Footer';
@@ -22,14 +23,18 @@ export default function App() {
         <Header />
         <main className="container wrapper flex-grow-1">
           <Routes>
+            <Route element={<ProtectedLayout />}>
+              {/* protected routes */}
+              <Route path={Endpoints.Users} element={<UsersPage />} />
+              <Route path={Endpoints.UserEdit} element={<EditUserPage />} />
+              <Route path={Endpoints.Statuses} element={<StatusesPage />} />
+              <Route path={Endpoints.Labels} element={<LabelsPage />} />
+              <Route path={Endpoints.Tasks} element={<TasksPage />} />
+            </Route>
+            {/* public routes */}
             <Route path={Endpoints.Home} element={<MainPage />} />
             <Route path={Endpoints.SignUp} element={<SignUpPage />} />
             <Route path={Endpoints.Login} element={<LoginPage />} />
-            <Route path={Endpoints.Users} element={<UsersPage />} />
-            <Route path={Endpoints.UserEdit} element={<EditUserPage />} />
-            <Route path={Endpoints.Statuses} element={<StatusesPage />} />
-            <Route path={Endpoints.Labels} element={<LabelsPage />} />
-            <Route path={Endpoints.Tasks} element={<TasksPage />} />
             <Route path={Endpoints.Wildcard} element={<NotFoundPage />} />
           </Routes>
         </main>
