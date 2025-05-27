1d90440bcc0192467df11c23f685f1e95763cb5f
Konstantin Korovin <cosmo.stan@yandex.com>
2025-05-27 22:37:00 +0300

    feat: add routes for statuses CRUD

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,7 +1,9 @@
 import sessionRouter from './session';
 import usersRouter from './users';
+import statusesRouter from './statuses';
 
 export default [
   sessionRouter,
   usersRouter,
+  statusesRouter,
 ];
