6cf7e22e630be8971373e8a78481226614145316
Konstantin Korovin <cosmo.stan@yandex.com>
2025-05-28 16:25:51 +0300

    feat: update types

--- src/types.tsx
+++ src/types.tsx
@@ -1,16 +1,34 @@
-export type User = {
-  id: number,
-  firstName: string,
-  lastName: string,
-  email: string,
-  createdAt: string,
-};
-
 export type Credentials = {
-  email: string,
-  password: string,
+  email: string;
+  password: string;
 };
 
-export type EntitiesUnion = User;
-export type TableNamesUnion = 'users';
-export type ColNamesUnion = 'id' | 'fullName' | 'email' | 'createdAt';
+export type User = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  createdAt: string;
+};
+
+export type Status = {
+  id: number;
+  name: string;
+  createdAt: string;
+};
+
+export type EntityMap = {
+  users: User;
+  statuses: Status;
+};
+
+export type TableNamesUnion = keyof EntityMap;
+
+type ColumnMap = {
+  users: Array<keyof User | 'fullName'>;
+  statuses: Array<keyof Status>;
+};
+
+export type TableColumns<T extends TableNamesUnion> = ColumnMap[T];
+
+export type TableRows<T extends TableNamesUnion> = EntityMap[T][];
