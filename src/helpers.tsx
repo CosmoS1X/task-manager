5bb8910763ee5b20ab5a2e296883b1feb1e88112
Konstantin Korovin <cosmo.stan@yandex.com>
2025-05-28 16:25:08 +0300

    feat: add 'capitalize' function to helpers

--- src/helpers.tsx
+++ src/helpers.tsx
@@ -1,3 +1,5 @@
-/* eslint-disable import/prefer-default-export */
-
 export const formatDate = (date: string) => new Date(date).toLocaleString();
+
+export const capitalize = (string: string) => (
+  `${string[0].toUpperCase()}${string.slice(1).toLowerCase()}`
+);
