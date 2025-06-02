d1dd61e85a0568415576f2b06bf2e4f5621f806c
Konstantin Korovin <cosmo.stan@yandex.com>
2025-06-02 16:40:52 +0300

    feat: add 'Label' model

---
+++ server/models/Label.ts
@@ -0,0 +1,22 @@
+import { Model } from '../database';
+
+export default class Label extends Model {
+  id!: number;
+
+  name!: string;
+
+  static get tableName() {
+    return 'labels';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['name'],
+      properties: {
+        id: { type: 'integer' },
+        name: { type: 'string', minLength: 1, maxLength: 255 },
+      },
+    };
+  }
+}
