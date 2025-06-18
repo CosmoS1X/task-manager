7724753c9d6a73ae935a83ca3b1b22116817ff10
Konstantin Korovin <cosmo.stan@yandex.com>
2025-06-18 20:10:01 +0300

    feat: add endpoints for labels

--- server/endpoints.ts
+++ server/endpoints.ts
@@ -7,10 +7,13 @@ enum Endpoints {
   CheckAuth = '/api/check-auth',
   Statuses = '/api/statuses',
   Status = '/api/statuses/:id',
+  Labels = '/api/labels',
+  Label = '/api/labels/:id',
 }
 
 export const getUserPath = (id: number) => `${Endpoints.Users}/${id}`;
 export const getStatusPath = (id: number) => `${Endpoints.Statuses}/${id}`;
+export const getLabelPath = (id: number) => `${Endpoints.Labels}/${id}`;
 export const getCheckEmailQueryString = (email: string) => `${Endpoints.CheckEmail}?email=${email}`;
 
 export default Endpoints;
