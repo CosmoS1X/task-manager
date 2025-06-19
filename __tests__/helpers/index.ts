b6e44c0e1d606d4ec0b76dc90ea21246d0714235
Konstantin Korovin <cosmo.stan@yandex.com>
2025-06-19 20:01:08 +0300

    test: add helpers for testing tasks

--- __tests__/helpers/index.ts
+++ __tests__/helpers/index.ts
@@ -16,7 +16,32 @@ export const createLabelData = () => ({
   name: faker.lorem.word(),
 });
 
+export const createTaskData = () => ({
+  name: faker.lorem.word(),
+  description: faker.lorem.text(),
+});
+
 export const getUserPath = (id: number) => `${Endpoints.Users}/${id}`;
 export const getStatusPath = (id: number) => `${Endpoints.Statuses}/${id}`;
 export const getLabelPath = (id: number) => `${Endpoints.Labels}/${id}`;
+export const getTaskPath = (id: number) => `${Endpoints.Tasks}/${id}`;
 export const getCheckEmailQueryString = (email: string) => `${Endpoints.CheckEmail}?email=${email}`;
+
+type FilterParams = {
+  status?: number;
+  executor?: number;
+  label?: number;
+  isCreator?: boolean;
+};
+
+export const buildQueryString = (filters: FilterParams) => {
+  const searchParams = new URLSearchParams();
+
+  Object.entries(filters).forEach(([key, value]) => {
+    if (value) {
+      searchParams.append(key, String(value));
+    }
+  });
+
+  return `${Endpoints.Tasks}?${searchParams.toString()}`;
+};
