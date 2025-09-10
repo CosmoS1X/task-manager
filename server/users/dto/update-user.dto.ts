b7849078cd4e4bccd6fe741f97545f03892b3a48
Konstantin Korovin <cosmo.stan@yandex.com>
2025-09-10 19:33:10 +0300

    feat: add DTOs for creating and updating users

---
+++ server/users/dto/update-user.dto.ts
@@ -0,0 +1,30 @@
+import { IsString, MinLength, MaxLength, IsOptional, IsEmail, ValidateIf } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsString()
+  @MinLength(2)
+  @MaxLength(255)
+  @IsOptional()
+  firstName?: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(255)
+  @IsOptional()
+  lastName?: string;
+
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+
+  @IsString()
+  @MaxLength(255)
+  @ValidateIf((obj) => obj.newPassword !== undefined)
+  currentPassword?: string;
+
+  @IsString()
+  @MinLength(5)
+  @MaxLength(255)
+  @IsOptional()
+  newPassword?: string;
+}
