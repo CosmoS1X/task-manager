3c42be77249576ed6578781ffc3517d8ad84e396
Konstantin Korovin <cosmo.stan@yandex.com>
2025-09-12 20:02:33 +0300

    feat: add auth feature to project via sessions

--- server/main.ts
+++ server/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { loggerConfig, corsConfig, validatorConfig } from './configs';
+import { loggerConfig, corsConfig, validatorConfig, sessionConfig } from './configs';
 import env from '../env';
 
 async function bootstrap() {
@@ -10,6 +10,7 @@ async function bootstrap() {
   app.enableCors(corsConfig);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe(validatorConfig));
+  app.use(sessionConfig);
 
   await app.listen(env.PORT);
 }
