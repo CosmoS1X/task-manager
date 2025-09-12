e4b0172dabf946b928f81376fdd41d3615a79da8
Konstantin Korovin <cosmo.stan@yandex.com>
2025-09-12 20:21:22 +0300

    feat: add login after registration and logout after user deletion

--- server/users/users.controller.ts
+++ server/users/users.controller.ts
@@ -9,7 +9,12 @@ import {
   ParseIntPipe,
   HttpCode,
   HttpStatus,
+  Session,
+  Res,
 } from '@nestjs/common';
+import type { Response } from 'express';
+import type { SessionData } from 'express-session';
+import { AuthService } from '@server/auth/auth.service';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -17,7 +22,10 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Get()
   async findAll(): Promise<User[]> {
@@ -30,8 +38,15 @@ export class UsersController {
   }
 
   @Post()
-  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
-    return this.usersService.create(createUserDto);
+  async create(
+    @Body() createUserDto: CreateUserDto,
+    @Session() session: SessionData,
+  ): Promise<User> {
+    const user = await this.usersService.create(createUserDto);
+
+    await this.authService.login(user.id, session);
+
+    return user;
   }
 
   @Patch(':id')
@@ -44,7 +59,15 @@ export class UsersController {
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  async delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
+  async delete(
+    @Param('id', ParseIntPipe) id: number,
+    @Session() session: SessionData,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<void> {
+    if (session.userId === id) {
+      await this.authService.logout(session, res);
+    }
+
     await this.usersService.delete(id);
   }
 }
