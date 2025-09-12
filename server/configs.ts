3fba8a1da05527c5dc66f1fbf7fda259c40a7359
Konstantin Korovin <cosmo.stan@yandex.com>
2025-09-12 20:01:29 +0300

    feat: add express session config

--- server/configs.ts
+++ server/configs.ts
@@ -1,4 +1,6 @@
 import { ConsoleLogger } from '@nestjs/common';
+import session from 'express-session';
+import env from 'env';
 
 // https://docs.nestjs.com/techniques/logger
 export const loggerConfig = {
@@ -30,3 +32,18 @@ export const validatorConfig = {
     enableImplicitConversion: true,
   },
 };
+
+export const sessionConfig = session({
+  name: 'taskmanager.sid',
+  secret: env.SESSION_KEY,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false, // 'true' doesn't work on render.com
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
+    httpOnly: true,
+    sameSite: 'lax', // 'none' doesn't work on render.com
+  },
+  unset: 'destroy',
+  rolling: true,
+});
