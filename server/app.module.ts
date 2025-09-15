5d09fff2474fb3dba20085f08996a079bd1f78f5
Konstantin Korovin <cosmo.stan@yandex.com>
2025-09-15 18:59:34 +0300

    feat: add statuses module with CRUD operations and logging

--- server/app.module.ts
+++ server/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { StatusesModule } from './statuses/statuses.module';
 
 @Module({
   imports: [
     DatabaseModule,
     UsersModule,
     AuthModule,
+    StatusesModule,
   ],
 })
 export class AppModule {}
