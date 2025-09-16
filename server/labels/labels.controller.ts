380f4c78bd7bffad576496387addad88b1191980
Konstantin Korovin <cosmo.stan@yandex.com>
2025-09-16 21:36:16 +0300

    feat: add labels controller

---
+++ server/labels/labels.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  ParseIntPipe,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@server/auth/guards/auth.guard';
+import { LabelsService } from './labels.service';
+import { Label } from './entities/label.entity';
+import { CreateLabelDto } from './dto/create-label.dto';
+import { UpdateLabelDto } from './dto/update-label.dto';
+
+@Controller('labels')
+@UseGuards(AuthGuard) // Protect all routes
+export class LabelsController {
+  constructor(private readonly labelsService: LabelsService) {}
+
+  @Get()
+  async findAll(): Promise<Label[]> {
+    return this.labelsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number): Promise<Label> {
+    return this.labelsService.findById(id);
+  }
+
+  @Post()
+  async create(@Body() createLabelDto: CreateLabelDto): Promise<Label> {
+    return this.labelsService.create(createLabelDto);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateLabelDto: UpdateLabelDto,
+  ): Promise<Label> {
+    return this.labelsService.update(id, updateLabelDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    await this.labelsService.delete(id);
+  }
+}
