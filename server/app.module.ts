c093ea76f689feee7091df2ede600c4528af8b31
Konstantin Korovin <cosmo.stan@yandex.com>
2025-09-16 21:48:49 +0300

    feat: add labels module with CRUD operations and logging

--- server/app.module.ts
+++ server/app.module.ts
@@ -3,6 +3,7 @@ import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { StatusesModule } from './statuses/statuses.module';
+import { LabelsModule } from './labels/labels.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { StatusesModule } from './statuses/statuses.module';
     UsersModule,
     AuthModule,
     StatusesModule,
+    LabelsModule,
   ],
 })
 export class AppModule {}
