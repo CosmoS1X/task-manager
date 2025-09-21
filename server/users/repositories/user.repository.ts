d38ffa3e7fa3b0f472d031625cccc15b98d238b7
Konstantin Korovin <cosmo.stan@yandex.com>
2025-09-21 18:33:14 +0300

    fix: add user existence check to create method

--- server/users/repositories/user.repository.ts
+++ server/users/repositories/user.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable, ForbiddenException } from '@nestjs/common';
+import { Injectable, ForbiddenException, ConflictException } from '@nestjs/common';
 import { BaseRepository } from '@server/common/repositories/base.repository';
 import { User } from '../entities/user.entity';
 import { CreateUserDto } from '../dto/create-user.dto';
@@ -13,6 +13,15 @@ export class UserRepository extends BaseRepository<User> {
   }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
+    const existingUser = await this.findByEmail(createUserDto.email);
+
+    if (existingUser) {
+      throw new ConflictException({
+        error: 'UserAlreadyExists',
+        message: 'User with this email already exists',
+      });
+    }
+
     return this.model.query().insertAndFetch(createUserDto);
   }
 
