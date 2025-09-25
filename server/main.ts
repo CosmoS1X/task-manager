45434d9e9555c4fe30fd1aebc551ee5d58f3069a
Konstantin Korovin <cosmo.stan@yandex.com>
2025-09-25 16:23:12 +0300

    feat: serve static files from server for production

--- server/main.ts
+++ server/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import type { Request, Response, NextFunction } from 'express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 import { loggerConfig, corsConfig, validatorConfig, sessionConfig } from './configs';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
@@ -7,13 +10,25 @@ import { ObjectionFilter } from './common/filters/objection.filter';
 import env from '../env';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, loggerConfig);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, loggerConfig);
 
   app.use(sessionConfig);
   app.enableCors(corsConfig);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe(validatorConfig));
   app.useGlobalFilters(new AllExceptionsFilter(), new ObjectionFilter());
+
+  if (env.isProduction) {
+    const clientPath = join(__dirname, '..', 'client');
+
+    app.useStaticAssets(clientPath, { index: false });
+
+    app.use((req: Request, res: Response, next: NextFunction) => {
+      if (req.url.startsWith('/api/')) return next();
+
+      return res.sendFile(join(clientPath, 'index.html'));
+    });
+  }
 
   await app.listen(env.PORT);
 }
