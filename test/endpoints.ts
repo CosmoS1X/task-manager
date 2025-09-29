4472b66f63adb1463afa4313ba27965559c6f3f3
Konstantin Korovin <cosmo.stan@yandex.com>
2025-09-29 19:22:52 +0300

    test: add endpoints for testing

---
+++ test/endpoints.ts
@@ -0,0 +1,16 @@
+enum Endpoints {
+  Login = '/api/login',
+  Logout = '/api/logout',
+  Users = '/api/users',
+  User = '/api/users/:id',
+  CheckEmail = '/api/check-email',
+  CheckAuth = '/api/check-auth',
+  Statuses = '/api/statuses',
+  Status = '/api/statuses/:id',
+  Labels = '/api/labels',
+  Label = '/api/labels/:id',
+  Tasks = '/api/tasks',
+  Task = '/api/tasks/:id',
+}
+
+export default Endpoints;
