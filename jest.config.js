337601530fc621197646106ac5ed203ab2ee76f7
Konstantin Korovin <cosmo.stan@yandex.com>
2025-09-29 19:18:30 +0300

    chore: update jest config for nest e2e tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,40 @@
 module.exports = {
   preset: 'ts-jest',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/__tests__/setup.ts'],
-  testPathIgnorePatterns: ['/helpers/', '/setup.ts'],
-  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
-  moduleFileExtensions: ['ts', 'js'],
+  testRegex: '.*\\.(spec|e2e-spec)\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'server/**/*.(t|j)s',
+    '!server/main.ts',
+    '!server/**/*.module.ts',
+    '!server/**/*.dto.ts',
+    '!server/**/*.entity.ts',
+    '!server/types/**',
+    '!server/migrations/**',
+    '!server/database/**',
+  ],
+  coverageDirectory: './coverage',
+    coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  coverageReporters: ['text', 'lcov', 'html', 'text-summary'],
+  moduleNameMapper: {
+    '^@server/(.*)$': '<rootDir>/server/$1',
+    '^@client/(.*)$': '<rootDir>/client/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
+  modulePaths: ['<rootDir>'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+  ],
 };
