bfb54f864cf0a7e4df49d4ca4a79807b5b073fc1
Konstantin Korovin <cosmo.stan@yandex.com>
2025-09-30 18:16:25 +0300

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -9,23 +9,16 @@ module.exports = {
   },
   collectCoverageFrom: [
     'server/**/*.(t|j)s',
-    '!server/main.ts',
-    '!server/**/*.module.ts',
-    '!server/**/*.dto.ts',
-    '!server/**/*.entity.ts',
-    '!server/types/**',
-    '!server/migrations/**',
-    '!server/database/**',
+  ],
+  coveragePathIgnorePatterns: [
+    'server/main.ts',
+    '.dto.ts$',
+    '/types/',
+    '/migrations/',
+    '/database/',
+    'constructor.*private.*readonly',
   ],
   coverageDirectory: './coverage',
-    coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
   coverageReporters: ['text', 'lcov', 'html', 'text-summary'],
   moduleNameMapper: {
     '^@server/(.*)$': '<rootDir>/server/$1',
