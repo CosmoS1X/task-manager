3115f0a7b8fb06cd4ecc8150b55e0c7dc4da30f1
Konstantin Korovin <cosmo.stan@yandex.com>
2026-01-26 18:48:34 +0300

    refactor: update users module, service and controller to use TypeORM

--- server/users/users.module.ts
+++ server/users/users.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 import { AuthModule } from '@server/auth/auth.module';
-import { DatabaseModule } from '../database/database.module';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UserRepository } from './repositories/user.repository';
 
 @Module({
-  imports: [DatabaseModule, AuthModule],
+  imports: [TypeOrmModule.forFeature([User]), AuthModule],
   controllers: [UsersController],
   providers: [UsersService, UserRepository],
   exports: [UsersService],
