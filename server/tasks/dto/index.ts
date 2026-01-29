944476dd66ad2d34c717a93302bcacf883e20faf
Konstantin Korovin <cosmo.stan@yandex.com>
2026-01-29 16:08:14 +0300

    feat: add index file for task DTOs

---
+++ server/tasks/dto/index.ts
@@ -0,0 +1,5 @@
+import { CreateTaskDto } from './create-task.dto';
+import { UpdateTaskDto } from './update-task.dto';
+import { TaskFilterDto } from './task-filter.dto';
+
+export { CreateTaskDto, UpdateTaskDto, TaskFilterDto };
