2376def49da6d30a3fadbb24df56fb4bce45dd56
Konstantin Korovin <cosmo.stan@yandex.com>
2026-02-04 19:12:18 +0300

    feat: add password verification function

--- server/lib/secure.ts
+++ server/lib/secure.ts
@@ -1,3 +1,9 @@
 import crypto from 'crypto';
 
-export default (password: string) => crypto.createHash('sha256').update(password).digest('hex');
+export function encrypt(password: string): string {
+  return crypto.createHash('sha256').update(password).digest('hex');
+}
+
+export function verifyPassword(password: string, hash: string): boolean {
+  return encrypt(password) === hash;
+}
