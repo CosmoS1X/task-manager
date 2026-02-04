6e3edc84ae13b687c836ff7ceee7098a2a9c1af2
Konstantin Korovin <cosmo.stan@yandex.com>
2026-02-04 19:28:12 +0300

    fix: fix linter error

--- server/users/users.module.ts
+++ server/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
 import { AuthModule } from '@server/auth/auth.module';
+import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UserRepository } from './repositories/user.repository';
