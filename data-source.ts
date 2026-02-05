c12925cc53ea3d4537d6a2859f43b4bd19e822d1
Konstantin Korovin <cosmo.stan@yandex.com>
2026-02-05 19:38:03 +0300

    chore: join database configs

--- data-source.ts
+++ data-source.ts
@@ -7,7 +7,7 @@ export const databaseConfig: DataSourceOptions = {
   type: 'postgres',
   host: env.PG_HOST || 'localhost',
   port: env.PG_PORT || 5432,
-  database: env.PG_DB,
+  database: env.isTest ? env.PG_DB_TEST : env.PG_DB,
   username: env.PG_USER,
   password: env.PG_PASSWORD,
   ssl: env.isProduction,
@@ -15,22 +15,12 @@ export const databaseConfig: DataSourceOptions = {
   entities: [path.join(__dirname, 'server', '**', '*.entity{.ts,.js}')],
   migrations: [path.join(__dirname, 'server', 'migrations', '*{.ts,.js}')],
   migrationsTableName: 'typeorm_migrations',
-  dropSchema: false,
-  synchronize: false,
+  dropSchema: env.isTest,
+  synchronize: env.isTest,
   logging: env.isDevelopment,
   extra: {
     charset: 'utf8mb4',
   },
 };
 
-export const testDatabaseConfig: DataSourceOptions = {
-  ...databaseConfig,
-  database: env.PG_DB_TEST || 'task_manager_test',
-  username: env.PG_USER_TEST || 'postgres',
-  password: env.PG_PASSWORD_TEST || 'postgres',
-  dropSchema: true,
-  synchronize: true,
-  logging: false,
-};
-
-export const AppDataSource = new DataSource(env.isTest ? testDatabaseConfig : databaseConfig);
+export const AppDataSource = new DataSource(databaseConfig);
